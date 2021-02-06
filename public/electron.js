e8c2f80b0addecc10a64e16853f25bc3bf5d140d
Sean Washington <hello@seanwash.com>
2021-02-06 09:56:31 -0800

    Updates
    - Remove rescripts
    - Replace yarn with npm
    - Add electron wrapper

---
+++ public/electron.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    height: 900,
+    width: 1400,
+    webPreferences: {
+      // TODO: Why do we need to set this here? Is it a possible security concern?
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+}
+
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
