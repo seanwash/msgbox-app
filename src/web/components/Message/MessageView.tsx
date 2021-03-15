843d0d0b2201f2eb32553b5577c183d67fb3a445
Sean Washington <hello@seanwash.com>
2021-03-15 14:12:02 -0700

    Style no selected message message

--- src/web/components/Message/MessageView.tsx
+++ src/web/components/Message/MessageView.tsx
@@ -100,7 +100,23 @@ const MessageView: React.FC = () => {
       </div>
     </div>
   ) : (
-    <div className="p-4">Select a message.</div>
+    <div className="flex h-full items-center justify-center p-4 text-2xl text-gray-400">
+      <svg
+        className="h-6 w-6 mr-2"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M10 19l-7-7m0 0l7-7m-7 7h18"
+        />
+      </svg>
+      Select a message
+    </div>
   );
 };
 
